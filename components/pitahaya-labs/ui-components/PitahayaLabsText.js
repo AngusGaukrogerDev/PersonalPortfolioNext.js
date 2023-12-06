228c5a8dbf4c28974e1f12b69c48042813b33d32
AngusGaukrogerDev <agaukroger77@gmail.com>
2023-12-06 18:13:04 +0000

    pedantic much

--- components/pitahaya-labs/ui-components/PitahayaLabsText.js
+++ components/pitahaya-labs/ui-components/PitahayaLabsText.js
@@ -2,13 +2,13 @@ import ButtonExternalLink from "@/components/shared/ButtonExternalLink";
 
 const PitahayaLabsText = () => {
     return(
-        <div className="h-full md:w-full gap-3 flex flex-col justify-start items-start md:gap-5 xl:gap-14 xl:flex-row xl:justify-evenly xl:items-center">
-            <div className="xl:w-1/2 xl:flex xl:flex-col xl:justify-center">
+        <div className="h-full md:w-full gap-3 flex flex-col justify-start items-start md:gap-5 xl:gap-14 xl:flex-row xl:justify-evenly xl:items-center 2xl:gap-3">
+            <div className="xl:w-1/2 xl:flex xl:flex-col xl:justify-center 2xl:w-full">
                 <h2>Pitahaya Labs</h2>
 
             </div>
             
-            <div className="flex flex-col gap-3 md:gap-5 xl:w-full xl:h-auto">
+            <div className="flex flex-col gap-3 md:gap-5 xl:w-full xl:h-auto 2xl:w-full">
                 <h3 className="text-anti-flash_white">My Professional Web Development Services Business</h3>
                 <p >In September 2023 I founded Pitahaya Labs - A Web Development Business specialising in professional contracting services.</p>
                 <ButtonExternalLink 
