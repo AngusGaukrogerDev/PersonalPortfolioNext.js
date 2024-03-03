0ca8f2be2a79e5c1642075788a678d695001188e
AngusGaukrogerDev <agaukroger77@gmail.com>
2024-03-03 21:29:55 +0000

    date change

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
         <Link href={"/"} className="w-full h-12 mt-16 mb-16 flex flex-col justify-center items-center">
             <h4 className="text-anti-flash_white">© Angus Gaukroger - {new Date().getFullYear()} </h4> 
         </Link>
+        
     );
 }
 export default Footer;
